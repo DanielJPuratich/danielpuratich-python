388168dcdc4f778df46c6cf40fe443df1e78d442
ubuntustudent <20taylormatthew@bprep.org>
2018-09-07 09:18:18 -0700

    School

---
+++ Unit1Lab/Unit1Lab.py
@@ -0,0 +1,42 @@
+
+print ("What is your favorite color survey")
+print("Blue is 1")
+print ("Green is 2")
+print ("Red is 3")
+print ("Enter number 4 in order to finish the survey and see the results")
+print ("")
+
+num1 = int(input("Put in the number coresponding with your favorite color: "))
+
+int(Green) == 0
+int(Blue) == 0
+int (Red) == 0
+
+
+if num1 == 1:
+    print ('Your favorite color is Blue')
+    goto (1)
+
+
+elif num1 == 2:
+    print ('Your Favorite Color is Green')
+
+
+elif num1 ==3:
+    print ("Your favorite color is Red")
+
+elif num1 == 4:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
