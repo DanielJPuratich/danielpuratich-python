8d500fb98c478126589c7e20c2ed095141177baa
ubuntustudent <20taylormatthew@bprep.org>
2018-09-11 08:33:27 -0700

    School Push September 11, 2018

---
+++ Unit2ALab/Unit2ALab.py
@@ -0,0 +1,19 @@
+
+print ("3 + 4.5 is ")
+print (3 + 4.5)
+print ()
+
+myString = input ("Any Number: ")                      #Input number into variable
+print ("Your number + 5.5 is")
+print (float(5.5) + float(myString))                   #Add inputted number with 5.5 and then print it
+
+i = 100
+j = 200
+x = 300
+y = 100
+
+print ()
+print ((i == j) or (i > x))
+print ((i < j) and (i >= y))
+print ((i != j) and (i != x))
+print ((i < j) and (i < x) and (i <y))
