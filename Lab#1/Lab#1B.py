9bbc1324505782bc5c66f3c6a9a01189db719f94
ubuntustudent <20taylormatthew@bprep.org>
2018-09-28 08:34:09 -0700

    First Lab!

--- Lab#1/Lab#1B.py
+++ Lab#1/Lab#1B.py
@@ -3,82 +3,66 @@ def Main () :
     print ("You are " + yearsInSchool(schoolYearNum))
     print ()
 
+    check3 = str("no")
+    check4 = str("no")
+    check5 = str("no")
+    check6 = str("no")
+
     num1 = float(input ("Put in your best grade? - "))
-    check2 = str("Do you want to put in another grade? - ")
+    check2 = str(input("Do you want to put in another grade? - "))
 
     if str(check2) == "yes" :
         num2 = float(input ("Put in your second best grade? - "))
-        inum2 = int(1)
-        check3 = str("Do you want to put in another grade? - ")
+        check3 = str(input("Do you want to put in another grade? - "))
     elif str(check2) == "Yes" :
         num2 = float(input ("Put in your second best grade? - "))
-        inum2 = int(1)
-        check3 = str("Do you want to put in another grade? - ")
+        check3 = str(input("Do you want to put in another grade? - "))
     else:
-        inum2 = int(0)
-        inum3 = int (0)
-        inum4 = int (0)
-        inum5 = int (0)
-        inum6 = int (0)
+        num2 = float(num1)
 
     if str(check3) == "yes" :
         num3 = float(input ("Put in your third best grade? - "))
-        inum3 = int(1)
-        check4 = str("Do you want to enter another grade? - ")
+        check4 = str(input("Do you want to enter another grade? - "))
     elif str(check3) == "Yes" :
         num3 = float(input ("Put in your third best grade? - "))
-        inum3 = int(1)
-        check4 = str("Do you want to enter another grade? - ")
+        check4 = str(input("Do you want to enter another grade? - "))
     else:
-        inum3 = int (0)
-        inum4 = int (0)
-        inum5 = int (0)
-        inum6 = int(0)
+        num3 = float((num1 + num2) / (2))
 
     if str(check4) == "yes" :
         num4 = float(input ("Put in your fourth best grade? - "))
-        inum4 = int(1)
-        check5 = str("Do you want to enter another grade? - ")
+        check5 = str(input("Do you want to enter another grade? - "))
     elif str(check4) == "Yes" :
         num4 = float(input ("Put in your fourth best grade? - "))
-        inum4 = int(1)
-        check5 = str("Do you want to enter another grade? - ")
+        check5 = str(input("Do you want to enter another grade? - "))
     else:
-        inum4 = int (0)
-        inum5 = int (0)
-        inum6 = int(0)
+        num4 = float((num1 + num2 + num3) / (3))
 
     if str(check5) == "yes" :
         num5 = float(input ("Put in your fifth best grade? - "))
-        inum5 = int(1)
-        check6 = str("Do you want to enter another grade? - ")
+        check6 = str(input("Do you want to enter another grade? - "))
     elif str(check5) == "Yes" :
         num5 = float(input ("Put in your fifth best grade? - "))
-        inum5 = int(1)
-        check6 = str("Do you want to enter another grade? - ")
+        check6 = str(input("Do you want to enter another grade? - "))
     else:
-        inum5 = int (0)
-        inum6 = int(0)
+        num5 = float((num1 + num2 + num3 + num4) / (4))
 
     if str(check6) == "yes" :
         num6 = float(input ("Put in your sixth best grade? - "))
-        inum6 = int(1)
-        check7 = str("Do you want to enter another grade? - ")
-    elif str(check5) == "Yes" :
+    elif str(check6) == "Yes" :
         num6 = float(input ("Put in your sixth best grade? - "))
-        inum6 = int(1)
-        check7 = str("Do you want to enter another grade? - ")
     else:
-        inum6 = int(0)
-#THIS IS WHERE MODIFIED CODE STARTS, NEED TO UPDATE THINGS THAT SEND TO FUNCTIONS AND FUNCTIONS TO PROCESS IT
+        num6 = float((num1 + num2 + num3 + num4 + num5) / (5))
 
-    print ("Your grade average is " + calcAverageGrade(num1, num2, num3, num4) + "%.")
+#THIS IS WHERE MODIFIED CODE STARTS, NEED TO UPDATE THINGS THAT SEND TO FUNCTIONS AND FUNCTIONS TO PROCESS IT========
+
+    print ("Your grade average is " + calcAverageGrade(num1, num2, num3, num4, num5, num6) + "%.")
     print ()
 
-    print ("Your letter grade average is " + getLetterGrade (num1, num2, num3, num4) + ".")
+    print ("Your letter grade average is " + getLetterGrade (num1, num2, num3, num4, num5, num6) + ".")
     print ()
 
-    print ("You are " + passOrFail(num1, num2, num3, num4) + ".")
+    print ("You are " + passOrFail(num1, num2, num3, num4, num5, num6) + ".")
 #============================================================================================================
 def yearsInSchool (numGrade) :
     if numGrade == 9:
@@ -92,21 +76,31 @@ def yearsInSchool (numGrade) :
     else:
         return (str("not in highschool."))
 #============================================================================================================
-def calcAverageGrade (num1, num2, num3, num4) :
-    List1 = [0] * 4
+def calcAverageGrade (num1, num2, num3, num4, num5, num6) :
+    List1 = [0] * 6
     List1 [0] = num1
     List1 [1] = num2
     List1 [2] = num3
     List1 [3] = num4
-
+    List1 [4] = num5
+    List1 [5] = num6
+    #NEED TO ADD WAY FOR THING TO POP THE RIGHT AMMOUNT OF NUMBERS (MAYBE SOME SORT OF LOOP)==========================
     ListL = len (List1)
-    ListA = List1.pop (0) + List1.pop (0) + List1.pop (0) + List1.pop (0)
+    ListA = List1.pop (0) + List1.pop (0) + List1.pop (0) + List1.pop (0) + List1.pop (0) + List1.pop (0)
     Gpa = ListA / ListL
     Gpa2 = str(Gpa)
     return (Gpa2)
 #============================================================================================================
-def getLetterGrade (num1, num2, num3, num4) :
-    Avg = (num1 + num2 + num3 + num4) / 4
+def getLetterGrade (num1, num2, num3, num4, num5, num6) :
+    List1 = [0] * 6
+    List1 [0] = num1
+    List1 [1] = num2
+    List1 [2] = num3
+    List1 [3] = num4
+    List1 [4] = num5
+    List1 [5] = num6
+    ListL = len (List1)
+    Avg = (num1 + num2 + num3 + num4 + num5 + num6) / ListL
 
     if int(Avg) > 90:
        return (str("A"))
@@ -123,8 +117,8 @@ def getLetterGrade (num1, num2, num3, num4) :
     else:
         return (str("F"))
 
-def passOrFail (num1, num2, num3, num4) :
-    Avg = (num1 + num2 + num3 + num4) / 4
+def passOrFail (num1, num2, num3, num4, num5, num6) :
+    Avg = (num1 + num2 + num3 + num4 + num5 + num6) / 6
     if int(Avg) > 70:
         return (str("passing"))
     else:
