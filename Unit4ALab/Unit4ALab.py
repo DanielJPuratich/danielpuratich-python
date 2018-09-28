63cf68c4e509034449e2e5fe676db9f1fd697209
ubuntustudent <20taylormatthew@bprep.org>
2018-09-28 09:19:48 -0700

    g

---
+++ Unit4ALab/Unit4ALab.py
@@ -0,0 +1,37 @@
+myString = str(input("Enter any string - "))
+print ("Your string is - " + myString )
+print()
+print ("Your string without vowels is - " + deVowel(myString))
+grades = [95, 10, 4, 200]
+print ("Your average grades using a for loop equals " + calcAverageGrade(grades))
+
+
+def deVowel () :
+    #this is where it removes vowels using a for statement and then returns result
+    #will need if/else and and statements
+
+def calcAverageGrade () :
+    #use this to calculate average grades, add grades up using a for loop
+
+
+
+
+
+
+
+
+#====================================================================================================
+#not part of lab, but notes for it - comment this ut when working and delete it when done
+#l = [10,20,30]
+#sum = int(0)
+#for num in l :
+#    sum = sum + num
+#print (sum)
+
+#stringA = "ABCD ABCD"
+#stringB = ""
+#for x in stringA :
+#    if x != "A" :
+#        stringB = stringB + x
+#print (stringA)
+#print (stringB)
