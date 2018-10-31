09b9caddaf8ef23c0be64dc64fc4177c683dac55
ubuntustudent <20taylormatthew@bprep.org>
2018-10-31 10:07:08 -0700

    e

---
+++ diceSelectiveAdding/diceSelectiveAdding.py
@@ -0,0 +1,5 @@
+# this challenge should do:
+# make user select how big of a grid of dice they want
+# then allow the user to be able to add up any square in the grid
+# do this by taking inputs of corners based on "how far down" "hw far over"
+# then adding all the dice inside this small square
