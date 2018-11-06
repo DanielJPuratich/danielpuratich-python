68a01909547d06cbc830ee9bfe5dfe14044024a4
ubuntustudent <20taylormatthew@bprep.org>
2018-11-06 08:39:40 -0800

    e

---
+++ probabilityDiceRolling/probabilityDiceRolling.py
@@ -0,0 +1,29 @@
+from random import randint
+def main () :
+    print ("The program will run unti it gets the number you input 5 times in a row")
+    num = int(input("Input a number between 1 and 6 - "))
+    x = 0
+    times = 0
+    while x==0 :
+        times = times + 1
+        b = randint(1,6)
+        if b==num :
+            c = randint(1,6)
+            if c==num :
+                d = randint(1,6)
+                if d==num :
+                    e = randint(1,6)
+                    if e==num :
+                        f = randint(1,6)
+                        if f==num :
+                            yay (num, times)
+                            break
+
+def yay (num, times) :
+    print ("It took " + str(times) + " to roll a " + str(num))
+
+
+
+
+
+main ()
