a66c90c7868ad51c680477a95dc6d7eb83f35109
ubuntustudent <20taylormatthew@bprep.org>
2019-01-09 10:11:08 -0800

    e

--- SuperBreakout/Main.py
+++ SuperBreakout/Main.py
@@ -49,19 +49,62 @@ def Menu () :
             screen.fill(black)
             p.display.update()
             randLvl()
+            for x in range(0,100000) :
+                p.display.update()
         for event in p.event.get():
             if event.type == p.QUIT:
                 t = 'False'
 
 
 def randLvl () :
+    black = (0,0,0)
+    screen.fill(black)
+    p.display.update()
+
+    count = randint(1,5)
+    if count >= 1 :
+        Row1 = [randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5)]
+        List = [Row1]
+        if count >= 2 :
+            Row2 = [randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5)]
+            List = [Row1,Row2]
+            if count >= 3 :
+                Row3 = [randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5)]
+                List = [Row1,Row2,Row3]
+                if count >= 4 :
+                    Row4 = [randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5)]
+                    List = [Row1,Row2,Row3,Row4]
+                    if count >= 5 :
+                        Row5 = [randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5),randint(1,5)]
+                        List = [Row1,Row2,Row3,Row4,Row5]
+    y = 0
+    red5 = (221, 11, 56)
+    purple4 = (168, 11, 221)
+    blue3 = (11, 53, 221)
+    green2 = (11, 221, 144)
+    yellow1 = (193, 221, 11)
+
+    for row in List :
+        x = 0
+        for value in row :
+            if value==1 :
+                color = yellow1
+            elif value==2 :
+                color = green2
+            elif value==3 :
+                color = blue3
+            elif value==4 :
+                color = purple4
+            elif value==5 :
+                color = red5
+            p.draw.rect(screen, color, (x*50.5,y*26,50,25), 0 )
+            x = x + 1
+        y = y + 1
     p.display.update()
 
 
 
 
 
-
 Menu()
-#baseLoop()
 p.quit()
